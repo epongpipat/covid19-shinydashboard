c4b6d06e8eba49b4643d45d16cf1e0b08dce65ea
epongpipat <epongpipat@gmail.com>
2021-01-17 15:21:50 -0600

    fix county data for dallas and los angeles

--- get_data_dallas.R
+++ get_data_dallas.R
@@ -13,7 +13,7 @@ df_temp <- readxl::read_excel(temp_path, skip = 2, col_names = TRUE) %>%
          date = stringr::str_remove_all(date, "\n"),
          date = stringr::str_remove_all(date, "[*]"),
          date = stringr::str_squish(date),
-         date = as.Date(paste0("2020-", date)),
+         date = as.Date(date, "%m-%d-%Y"),
          value = c(NA, diff(value)),
          metric = "confirmed")
 df_dallas <- df_temp
@@ -23,17 +23,20 @@ rm(df_temp)
 download.file("https://dshs.texas.gov/coronavirus/TexasCOVID19DailyCountyFatalityCountData.xlsx", temp_path)
 df_temp <- readxl::read_excel(temp_path, skip = 2, col_names = TRUE) %>%
   rename(region = `County Name`) %>%
-  filter(region == "Dallas") %>%
+  mutate(region = tolower(region)) %>%
+  filter(region == "dallas") %>%
   tidyr::pivot_longer(., -region, names_to = "date", values_to = "value") %>%
   mutate(date = stringr::str_remove(date, "Fatalities "),
-         date = paste0("2020-", date),
-         date = as.Date(date),
+         date = as.Date(date, "%m-%d-%Y"),
          metric = 'deaths',
-         value = c(NA, diff(value)))
+         value = c(NA, diff(value)),
+         region = "Dallas")
 df_dallas <- rbind(df_dallas, df_temp)
 rm(df_temp)
 
 ### tests ----
+
+
 download.file("https://dshs.texas.gov/coronavirus/TexasCOVID-19CumulativeTestsOverTimebyCounty.xlsx", temp_path)
 df_temp <- readxl::read_excel(temp_path, skip = 1, col_names = T) %>%
   rename(region = `County`) %>%
@@ -45,12 +48,26 @@ df_temp <- readxl::read_excel(temp_path, skip = 1, col_names = T) %>%
          date = as.Date(lubridate::parse_date_time(paste0(date, ", 2020"), orders = "bd,y")),
          value = stringr::str_remove_all(value, "-"),
          value = as.numeric(value),
-         value = c(NA, diff(value)),
+         #value = c(NA, diff(value)),
          metric = 'tests')
+
+download.file("https://dshs.texas.gov/coronavirus/TexasCOVID-19CumulativeTestsbyCounty.xlsx", temp_path)
+df_temp2 <- readxl::read_excel(temp_path, skip = 1, col_names = T) %>%
+  rename(region = `County`) %>%
+  filter(region == "Dallas") %>%
+  as.matrix() %>%
+  as.data.frame() %>%
+  tidyr::pivot_longer(., -region, names_to = "date", values_to = "value") %>%
+  mutate(date = as.Date(as.numeric(date), origin = "1899-12-30"),
+         value = as.numeric(value),
+         metric = 'tests')
+df_temp <- rbind(df_temp, df_temp2) %>%
+  mutate(value = c(NA, diff(value)))
+rm(df_temp2)
 df_dallas <- rbind(df_dallas, df_temp)
 rm(df_temp)
 
-# hospitalization
+# hospitalization ----
 # this is for DFW metroplex
 download.file("https://dshs.texas.gov/coronavirus/TexasCOVID-19HospitalizationsOverTimebyTSA.xlsx", temp_path)
 df_temp <- readxl::read_excel(temp_path, skip = 2, col_names = T) %>%
--- get_data_la.R
+++ get_data_la.R
@@ -1,11 +1,16 @@
 df_la <- NULL
-df_temp <- read.csv("https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/latimes-county-totals.csv") %>%
+
+temp_path <- tempfile()
+
+download.file("https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/latimes-county-totals.csv", temp_path)
+df_temp <- read.csv(temp_path) %>%
   filter(county == "Los Angeles") %>%
   select(date, region = county, confirmed = new_confirmed_cases, deaths = new_deaths) %>%
   tidyr::pivot_longer(., -c(date, region), names_to = 'metric')
 df_la <- df_temp
 
-df_temp <- read.csv("https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/cdph-hospital-patient-county-totals.csv") %>%
+download.file("https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/cdph-hospital-patient-county-totals.csv", temp_path)
+df_temp <- read.csv(temp_path) %>%
   filter(county == "Los Angeles") %>%
   select(date, 
          region = county, 
@@ -13,4 +18,4 @@ df_temp <- read.csv("https://raw.githubusercontent.com/datadesk/california-coron
          icu = icu_positive_patients) %>%
   tidyr::pivot_longer(., -c(date, region), names_to = 'metric')
 df_la <- rbind(df_la, df_temp)
-rm(df_temp)
+rm(df_temp, temp_path)
